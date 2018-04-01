c6d4457157dc8c7eab0dd3f0f4ae7c1434a96bfd
softservemike <mykhailodulyba@gmail.com>
2018-04-01 13:21:30 +0000

    done

--- js/script.js
+++ js/script.js
@@ -1,191 +1,58 @@
-// for (let i = 100; i > 0; i--) {
-//     if (i % 2) {
-//         continue;
-//     }
-//     prn(i);
-// }
+//Recursia approach
 
-// let correctData = false;
-// let someNum;
-// while (!correctData) {
-//     someNum = parseFloat(prompt('Enter a number'));
-//     if (!isNaN(someNum)) {
-//         correctData = true;
-//     }
-// }
-
-
-
-// let correctData = false;
-// let someNum;
-// do {
-//     someNum = parseFloat(prompt('Enter a number'));
-//     if (!isNaN(someNum)) {
-//         correctData = true;
-//     }
-// } while (!correctData);
-
-
-//function prn(val) {
-//    document.write(val + '<br>');
-//}
-
-// function getAge(age) {
-//     // if (age >= 18) {
-//     //     return 'Adult';
-//     // } 
-//     // return 'Young';
-//     return age >= 18 ? 'Adult' 
-//             : age < 0 ? 'Incorrect' : 'Young';
-// }
-
-// prn(getAge(20));
-// prn(getAge(-20));
-// prn(getAge(2));
-
-// let cars = ['Bugatti Veyron', 'Ferrari F50', 'McLaren F1',
-//     'ZAZ Lanos'];
-    
-// prn(cars.length);
-
-// cars.unshift('Tesla Model X');
-
-// prn(cars.length);
-
-// // for (let i = 0; i < cars.length; i++) {
-// //     if (cars[i] === undefined) {
-// //         continue;
-// //     }
-// //     prn(cars[i]);
-// // }
-
-// // cars.forEach(prn);
-
-// cars.sort();
-// cars.reverse();
-
-// cars.forEach( function(el, idx) {
-//     prn(idx + ' ' + el);
-// });
-
-// // cars.forEach( (el, idx) => prn(idx + ' ' + el) );
-
-// let nums = [10,30,3,1,11,17,80,100,59];
-// prn(nums);
-// nums.sort( (a, b) => b - a );
-// prn(nums.join('<br>'));
-
-
-// let seasonCars = {};
-
-// seasonCars['Summer'] = 'Audi TT Cabrio';
-// seasonCars['Winter'] = 'Hummer H2';
-// seasonCars['Spring'] = 'Abrams A1';
-// seasonCars['Autumn'] = 'ZAZ Lanos';
-
-// // prn(seasonCars.length);
-
-// let counter = 0;
-// for (let key in seasonCars) {
-//     //counter++;
-//     prn(seasonCars[key]);    
-// }
-// prn(counter);
-
-//function beep () {
-//   prn(this.model + ': ' + 'Beep'); 
-//}
-
-//let car = {};
-//car.make = 'Audi'; // car['make'] = 'Audi';
-//car.model = 'TT Cabrio';
-//car.color = 'red';
-//car.price = 50000;
-//car.beep = beep;
-//car.beep();
-
-//let hummer = {};
-//hummer.model = 'H2';
-//hummer.beep = beep;
-//hummer.beep();
-
-//let lanos = {
-//    model: 'Lanos',
-//    make: 'ZAZ',
-//    color: 'grey',
-//    beep: beep
-//}
-//lanos.beep();
-
-//function Car(model, make, color) {
-//    this.model = model;
-//    this.make = make;
-//    this.color = color;
-//    this.beep = beep;
-//    return this;
-//}
-
-//let mcLaren = new Car('F1', 'McLaren', 'silver');
-//let ferrariF50 = new Car('F50', 'Ferrari', 'red');
-//mcLaren.beep();
-//ferrariF50.beep();
-
-//class SuperCar {
-//    constructor (model, make, color) {
-//        this.model = model;
-//        this.make = make;
-//        this.color = color;
-//    } 
-//    beep() {
-//       prn(this.model + ': ' + 'Beep-beep');  
-//    }
-//}
-
-//let bugattiVeyron = new SuperCar('Veyron', 'Bugatti', 'grey');
-//bugattiVeyron.beep();
-
-//let bugattiChiron = Object.create(bugattiVeyron);
-//bugattiChiron.model = 'Chiron';
-//bugattiChiron.beep();
-
-
-//function prn(val) {
-//    document.write(val + '<br>');
-//}
-
-
-
-const n = parseFloat(prompt('Enter n'));
-/*Recursia
-function fib(n) {
+const n = parseFloat(prompt('Recursia approach number'));
+/*
+    const re= fibRec(n);
+    document.write(re);
+    */
+function fibRec(n) {
     if (n < 2) { return n }
     else {
-        return fib(n - 1) + fib(n - 2);
+        return fibRec(n - 1) + fibRec(n - 2);
     }
 }
 
-
-for (let i = 0; i <= n; i++) {
-    document.write(fib(i));
+for (let i = 1; i <= n; i++) {
+    document.write(fibRec(i));
 }
 
-const result = fib(n);
-document.write(result);
-*/
+document.write('<br>');
 
-function fib(n) {
-  let a = 1;
-  let b = 1;
-  for (let i = 3; i <= n; i++) {
-    let c = a + b;
+//Cycle approach
+
+const s = parseFloat(prompt('Cycle approach number'));
+
+function fibCycle(s) {
+    let a = 1;
+    let b = 1;
+    for (let i = 3; i <= s; i++) {
+        let c = a + b;
         a = b;
         b = c;
-  }
-  return b;
+    }
+    return b;
 }
-for (let i = 1; i <= n; i++){
-    document.write(fib(i));
+for (let i = 1; i <= s; i++) {
+    document.write(fibCycle(i));
 }
-/*
-const result = fib(n);
-document.write(result);*/
+
+document.write('<br>');
+
+//Array approach
+const d = parseFloat(prompt('Array approach number'));
+
+function fibArray(d) {
+let array = [1, 1];
+    for (let i =3; i <= d; i++) {
+        array[2] = array[0] + array[1];
+        array[0]=array[1];
+        array[1]=array[2];
+    }
+    return array[1];
+}
+
+for (let i = 1; i <= d; i++) {
+    document.write(fibArray(i));
+}
+
+
